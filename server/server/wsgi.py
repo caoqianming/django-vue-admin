4a129a5ee0e339174a334c56eb024eedb42d4970
JackYin <jackyin@aijiatui.com>
2021-02-10 15:31:14 +0800

    修改Docker相关配置

--- server/server/wsgi.py
+++ server/server/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings_pro')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings_dev')
 
 application = get_wsgi_application()
