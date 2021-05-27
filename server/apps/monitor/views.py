10cb991ab27b3b0e72f7fdb87fd1645b24566292
caoqianming <caoqianming@foxmail.com>
2021-05-27 17:27:42 +0800

    增加异常hander 未完成

--- server/apps/monitor/views.py
+++ server/apps/monitor/views.py
@@ -3,6 +3,8 @@ import psutil
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
+import logging
+
 # Create your views here.
 
 class ServerInfo(APIView):
--- server/utils/response.py
+++ server/utils/response.py
@@ -3,6 +3,24 @@ from rest_framework.views import exception_handler
 from rest_framework.response import Response
 import rest_framework.status as status
 
+def my_exception_hander(exc, context):
+    """
+    自定义异常处理
+    :param exc: 异常
+    :param context: 抛出异常的上下文
+    :return: Response响应对象
+    """
+    # 调用drf框架原生的异常处理方法
+    response = exception_handler(exc, context)
+
+    if response is None:
+        view = context['view']
+        if isinstance(exc, DatabaseError) or isinstance(exc, RedisError):
+            # 数据库异常
+            logger.error('[%s] %s' % (view, exc))
+            response = Response({'message': '服务器内部错误'}, status=status.HT)
+
+    return response
 
 class BaseResponse(object):
     """
