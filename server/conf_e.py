d967e335e8c0a1d8a28941d2dac9570f6a9ed9e2
caoqianming <caoqianming@foxmail.com>
2023-11-03 17:49:26 +0800

    feat: 增加配置参数模板

---
+++ server/conf_e.py
@@ -0,0 +1,48 @@
+# 基本配置
+SECRET_KEY = 'xxxxxx'
+BASE_URL = 'http://127.0.0.1:8000'
+BASE_URL_IN = 'http://127.0.0.1:8000'
+BASE_URL_OUT = 'http://127.0.0.1:8000'
+
+# 邮箱配置
+EMAIL_HOST = 'xx'
+EMAIL_PORT = 25
+EMAIL_HOST_USER = 'xx'
+EMAIL_HOST_PASSWORD = 'xx'
+EMAIL_USE_TLS = True
+
+
+# 数据库配置
+DEBUG = True
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'xxx',
+        'USER': 'xx',
+        'PASSWORD': 'xx',
+        'HOST': 'xx',
+        'PORT': 'xx',
+    }
+}
+
+# 雪花ID
+SNOW_DATACENTER_ID = 1
+
+# 百度语音
+BD_SP_ID = 'xx'
+BD_SP_KEY = 'xx'
+BD_SP_SECRET = 'xx'
+
+# 运维相关
+SD_PWD = 'xx'
+BACKUP_PATH = '/home/xx/xx/xx'
+SH_PATH = '/home/xx/xx/xx/sh'
+
+# 微信相关
+WXMP_ENABLED = False
+WXMP_APPID = 'xx'
+WXMP_APPSECRET = 'xx'
+
+WX_ENABLED = False
+WX_APPID = 'xx'  # 测试号
+WX_APPSECRET = 'xx'
