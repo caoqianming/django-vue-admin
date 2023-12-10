19bd014259b3ea9d772e010dd7555218d615aaef
caoqianming <caoqianming@foxmail.com>
2023-12-10 17:02:43 +0800

    feat: 修改vuedist为dist

--- server/server/settings.py
+++ server/server/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = 'server.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['vuedist'],
+        'DIRS': ['dist'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
