2d8811f40f7b5adf397a0210e84b6e808c4d6d36
风中骄子 <chenfei_5201213@163.com>
2024-03-28 08:01:48 +0800

    add ftz模块

--- server/apps/ftz/models.py
+++ server/apps/ftz/models.py
@@ -31,7 +31,22 @@ class Lesson(SoftModel):
     type = models.CharField('课程类型', max_length=128, choices=lesson_type_choices, default='编程课')
     lesson_number = models.IntegerField('顺序', blank=False)
     version = models.CharField('版本号')
-    group = models.CharField('分组', max_length=128, blank=False, default="默认分组")
+    # group_id = models.ForeignKey(Group, on_delete=models.SET_NULL, verbose_name='分组')
+    course = models.ForeignKey(Course, on_delete=models.SET_NULL, verbose_name='课程', null=True)
+
+
+class Word(SoftModel):
+    """
+    单词表
+    """
+    pass
+
+
+class Grammar(SoftModel):
+    """
+    语法表
+    """
+    pass
 
 
 class Card(SoftModel):
@@ -54,11 +69,11 @@ class Card(SoftModel):
     )
     title = models.CharField('课时标题', max_length=128, blank=False)
     type = models.CharField('课程类型', max_length=128, choices=card_type_choices, default='编程课')
-    group = models.CharField('分组', max_length=128, blank=False, default="默认分组")
     status = models.IntegerField('状态', choices='', default=0)
     card_core_image = models.CharField('核心图', max_length=128, blank=True)
     topic = models.CharField('话题', max_length=128, blank=True)
     difficulty = models.CharField('难度', max_length=32, choices=card_difficulty_choices, blank=False, default='easy')
+    lessons = models.ManyToManyField(Lesson, blank=True, verbose_name='课时')
 
 
 class StudyMaterial(SoftModel):
@@ -75,3 +90,27 @@ class StudyMaterial(SoftModel):
     description = models.TextField('描述', blank=True)
     type = models.CharField('课程类型', max_length=128, choices=material_type_choices)
     context = models.TextField('素材内容')
+    # words = models.ManyToManyField(Word, blank=True, verbose_name='单词', related_name='words')
+    # grammars = models.ManyToManyField(Grammar, blank=True, verbose_name='语法', related_name='grammars')
+    # tags = models.ManyToManyField(Tag, blank=True, verbose_name='标签', related_name='tags')
+    cards = models.ManyToManyField(Card, blank=True, verbose_name='卡片', related_name='cards')
+
+
+class Group(SoftModel):
+    """
+    分组
+    """
+    name = models.CharField('名称', max_length=128, blank=False)
+    description = models.CharField('描述', max_length=512, blank=True)
+    # business = models.CharField('业务', max_length=128, blank=False)
+    # foreign_key_id = models.IntegerField('外键id', max_length=12, blank=True)
+    lessons = models.ManyToManyField(Lesson, verbose_name='课时')
+    cards = models.ManyToManyField(Card, verbose_name='卡片')
+
+
+class Tag(SoftModel):
+    """
+    标签表
+    """
+    name = models.CharField('名称', max_length=128, blank=False)
+    study_material = models.ManyToManyField(StudyMaterial, related_name='学习素材')
