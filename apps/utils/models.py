6b181c8890953a08db2e0e1cc99c299201002496
caoqianming <caoqianming@foxmail.com>
2024-06-07 09:49:17 +0800

    fix: base 对循环引用抛出异常

--- apps/utils/models.py
+++ apps/utils/models.py
@@ -62,13 +62,19 @@ class ParentModel(models.Model):
     def init_parent_link(self):
         link = []
         if self.parent is not None:
+            if self.parent == self:
+                raise Exception(f'{self.__class__.__name__}-{self.id}-存在循环引用')
             link = [self.parent.id] # 一级
             if self.parent.parent is not None: # 二级
+                if self.parent.parent == self:
+                    raise Exception(f'{self.__class__.__name__}-{self.id}-存在循环引用')
                 link.insert(0, self.parent.parent.id) 
                 if self.parent.parent.parent is not None: # 三级
+                    if self.parent.parent.parent == self:
+                        raise Exception(f'{self.__class__.__name__}-{self.id}-存在循环引用')
                     link.insert(0, self.parent.parent.parent.id)
                     if self.parent.parent.parent.parent is not None:
-                        raise Exception('最多支持四级')
+                        raise Exception(f'{self.__class__.__name__}-{self.id}-最多支持四级')
         return link
 
     def handle_parent(self, is_create:bool):
