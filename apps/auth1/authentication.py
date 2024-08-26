94774a6187fc7ecafb1383de1c6586a2ed917654
caoqianming <caoqianming@foxmail.com>
2024-08-26 15:07:23 +0800

    feat: base auth authenticate移除不必须的employee

--- apps/auth1/authentication.py
+++ apps/auth1/authentication.py
@@ -13,7 +13,7 @@ class CustomBackend(ModelBackend):
             return
         try:
             user = UserModel._default_manager.get(
-                Q(username=username) | Q(phone=username) | Q(employee__id_number=username))
+                Q(username=username) | Q(phone=username))
         except UserModel.DoesNotExist:
             # Run the default password hasher once to reduce the timing
             # difference between an existing and a nonexistent user (#20760).
