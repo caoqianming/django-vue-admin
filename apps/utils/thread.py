22e33c6d09c48b147c2fe1bc4c4fa031fb2e0c18
caoqianming <caoqianming@foxmail.com>
2024-10-09 13:46:38 +0800

    feat: base 添加全局线程池

--- apps/utils/thread.py
+++ apps/utils/thread.py
@@ -1,9 +1,17 @@
 import threading
 from apps.utils.decorators import auto_log
+from concurrent.futures import ThreadPoolExecutor
 
+# 创建全局线程池
+global_executor = ThreadPoolExecutor(max_workers=20)
 class MyThread(threading.Thread):
 
     @auto_log('MyThread', raise_exception=True, send_mail=True)
     def run(self) -> None:
         return super().run()
-    
+
+    def start_p(self):
+        """
+        使用线程池启动
+        """
+        global_executor.submit(self.run)
